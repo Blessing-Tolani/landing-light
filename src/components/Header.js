90219935b8b23f1dcebdd82b80c9575a77132987
Blessing-Tolani <ojoblessing999@gmail.com>
2022-02-10 15:17:50 +0100

    improvement: removed background color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <div className="h-24 sm:h-32 flex w-full items-center sm:justify-end pl-3 pr-3">
-      <div className="flex  justify-between w-full bg-blue-900 sm:w-4/5  xl:w-2/5">
-        <nav className=" bg-red-900 w-3/4 md:w-4/5  ">
+      <div className="flex  justify-between w-full sm:w-4/5  xl:w-2/5">
+        <nav className="  w-3/4 md:w-4/5  ">
           <ul className="flex text-xs sm:text-sm text-black justify-evenly items-end h-16 font-semibold pb-5 sm:pb-3">
             <li>TRIPS</li>
             <li>RECENTLY VIEWED</li>
